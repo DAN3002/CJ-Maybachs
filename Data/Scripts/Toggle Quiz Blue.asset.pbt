742675bacad1b6e3e454c527e56495f5bd0e3adf
DAN3002 <dinhanh300229@gmail.com>
2022-10-25 10:38:22 +0700

    Toggle modal

---
+++ Data/Scripts/Toggle Quiz Blue.asset.pbt
@@ -0,0 +1,10 @@
+Assets {
+  Id: 585636910412409301
+  Name: "Toggle Quiz Blue"
+  PlatformAssetType: 3
+  TextAsset {
+    CustomParameters {
+    }
+  }
+  SerializationVersion: 119
+}
---
+++ Data/Scripts/Toggle Quiz Blue.lua
@@ -0,0 +1,15 @@
+
+-- Custom 
+local PASSWORD_MODAL = script:GetCustomProperty("PasswordModal"):WaitForObject()
+local TRIGGER = script:GetCustomProperty("Trigger"):WaitForObject()
+
+
+TRIGGER.interactionLabel = "View Screen"
+
+function OnInteracted(trigger, player)
+	TRIGGER.interactionLabel = "werwer"
+	PASSWORD_MODAL.visibility = Visibility.FORCE_ON
+end
+
+TRIGGER.interactedEvent:Connect(OnInteracted)
+TRIGGER.isInteractable = true
